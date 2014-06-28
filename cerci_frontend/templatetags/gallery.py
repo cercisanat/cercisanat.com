655816f11ef2ae6c4c663dd46b3c4260b5f6fde7
Can Mustafa Özdemir <canmustafaozdemir@gmail.com>
2014-06-28 12:17:15 +0300

    carousel added

---
+++ cerci_frontend/templatetags/gallery.py
@@ -0,0 +1,21 @@
+from django import template
+from django.template import Context
+from django.template.loader import get_template
+register = template.Library()
+import re
+
+from cerci_content.models import Gallery
+
+
+@register.filter(name='gallery_embed')
+def gallery_embed(value):
+    matches = re.findall('\[gallery:(.*)\]', value)
+    galleries = Gallery.objects.filter(pk__in=matches)
+
+    for gallery in galleries:
+        context = Context({'gallery': gallery})
+        html = get_template('gallery.html').render(context)
+        value = value.replace('[gallery:%s]' % gallery.pk, html)
+    return value
+
+gallery_embed.is_safe = True
